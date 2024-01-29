8ddd08679b31bed8fb6e113640d8ee056eb1f5cf
Iurie Manea <manea.iurie@gmail.com>
2024-01-29 16:55:52 +0200

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Iurie Manea on 29.01.2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
